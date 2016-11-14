7c3c2373b421d21ef4ad8d63d4d85718170feed1
Alan Zawari <skilledDeveloper@gmail.com>
2016-11-14 15:50:30 -0800

    initial commit

---
+++ Dcor-test.js
@@ -0,0 +1,197 @@
+/**********************************************************
+	Dcor-test.js - Some use cases and tests for Dcor
+	
+	Alan Zawari 
+	May 2014
+	
+	Note: 
+		First define all sample functions (Block #1)
+		and then try different test cases one by one (Block #2)
+***********************************************************/
+
+//------------------------------------------------------------
+//Block #1. Define some sample functions...
+//------------------------------------------------------------
+
+function foo(a, b) {
+    console.log("inside foo. this:", this);
+    return "hello " + a + " " + b;
+}
+
+function bar(a, b, c) {
+    var d = 1;
+    return a + b + c + d;
+}
+
+var myLib = {
+    foo: function (s) {
+        return "myLib.foo says: " + s;
+    },
+    bar: 10
+};
+
+var MySinglton = function () {
+    this.a = 1024;
+};
+
+function MyCtor(c) {
+    var prvt = 9;
+    var d = c || 0;
+    this.a = "foo";
+    this.b = d + prvt + 1;
+    console.log("this.b: ", this.b); //10
+}
+
+function isPrime(value) {
+    var prime = value != 1; // 1 can never be prime
+    for (var i = 2; i < value; i++) {
+        console.log("Checking " + i);
+        if (value % i === 0) {
+            prime = false;
+            break;
+        }
+    }
+    return prime;
+}
+
+function recursiveFunc(n) {
+    return n > 1 ? recursiveFunc(n - 1) + "-Hi" : "Hi";
+}
+
+function lengthyOperation(n) {
+    console.log("lengthyOperation started. n:" + n);
+    var res = 0;
+    for (var i = 0; i < n; i++) {
+        for (var j = 0; j < n; j++) {
+            res += (i + j);
+        }
+    }
+    console.log("lengthyOperation completed. res:" + res);
+    return res;
+}
+
+//------------------------------------------------------------
+//Block #2: Decorate functions and call them
+//------------------------------------------------------------
+
+//************************************************************
+
+//normal call:
+foo("beautiful", "world"); 
+
+//decorate it:
+Dcor.logged("foo", function (fname, fargs, res) {
+    console.log("Log> function " + fname + " called with: ", fargs, " and returned: ", res);
+});
+
+//expect:
+foo("beautiful", "world"); //should be logged
+
+//************************************************************
+
+//normal call:
+new MyCtor();
+
+//decorate it:
+Dcor.decorate("MyCtor", [{
+    type: "safeConstructor"
+}]);
+
+//expect: 
+MyCtor(); //should be the same as new MyCtor().
+new MyCtor();
+console.log(typeof b == "undefined"); //global context shouldn't affected and therefore 'b' must not exist
+
+//************************************************************
+
+//normal call:
+lengthyOperation(30000); //should take about 5 seconds and UI is blocked
+console.log("hooray! lengthyOperation is done."); //no result until lengthyOperation is completed
+
+//decorate it:
+Dcor.decorate("lengthyOperation", [{
+    type: "asynch",
+    callback: function (fname, fargs, res, f, ns) {
+        console.log("Asynch> function " + fname + " called with: ", fargs, " and returned: ", res);
+    }
+}]);
+
+//expect: 
+lengthyOperation(30000); //asynch. shouldn't block the UI
+console.log("hooray! lengthyOperation is done."); //we should see this immediately
+
+//************************************************************
+
+//normal call:
+mySinglton1 = new MySinglton();
+mySinglton2 = MySinglton();
+console.log("mySinglton1 === mySinglton2?", mySinglton1 === mySinglton2) //false. two different instances
+
+//decorate it:
+Dcor.singleton("MySinglton");
+
+//expect: 
+mySinglton1 = new MySinglton();
+mySinglton2 = MySinglton();
+console.log("mySinglton1 === mySinglton2?", mySinglton1 === mySinglton2) //true. two instances are the same
+
+//************************************************************
+
+//normal call:
+isPrime(173); 
+isPrime(173); //both calls take the same amount of time
+
+//decorate it: with "cached" and "performanceLogged"
+Dcor.decorate("isPrim.*", [{
+    type: "cached",
+    callback: function (fromCache, fname, fargs, res, f, ns) {
+        if (fromCache) console.log("Cache> function " + fname + " called with: ", fargs, " and returned result: ", res + " from cache");
+        else console.log("Cache> function " + fname + " called with: ", fargs, " and stored result: ", res + " to cache");
+    }
+}, {
+    type: "performanceLogged",
+    callback: function (duration, fname, fargs, res, f, ns) {
+        console.log("Perf> function " + fname + " called with: ", fargs, " and ran for:", duration + "ms");
+    }
+}]);
+
+//expect: isPrime is now cached and performanceLogged
+isPrime(173); //must be calculated and show the execution time
+isPrime(173); //this time must be retrieved from cache with faster execution time
+
+//************************************************************
+
+//normal call:
+recursiveFunc(25); //25 recursion
+
+//decorate it:
+Dcor.cached("recursiveFunc", function (fromCache, fname, fargs, res, f, ns) {
+    if (fromCache) console.log("Cache> function " + fname + " called with: ", fargs, " and returned result: ", res + " from cache");
+    else console.log("Cache> function " + fname + " called with: ", fargs, " and stored result: ", res + " to cache");
+});
+
+//expect:
+recursiveFunc(25); //25 recursion
+recursiveFunc(10); //no recursion. should use cache
+recursiveFunc(17); //no recursion. should use cache
+
+//************************************************************
+
+//normal call:
+myLib.foo("hi"); //runs normally. no authorization required
+
+//decorate it:
+Dcor.secured("foo", function (fname, fargs) {
+    var authorized = false; //check whether it is allowed to call this function...
+    if (!authorized) 
+		//return false to silently prevent running this function 
+		throw new Error("Not authorized to run function " + fname + " with: " + fargs); 
+    else 
+		return true;
+    //console.log("Authorization> Not authorized to run function " + fname + " with: ", fargs);
+}, [myLib]);
+
+//expect:
+myLib.foo("hi"); //won't run. should give authorization error
+
+//************************************************************
---
+++ Dcor.js
@@ -0,0 +1,206 @@
+/**********************************************************
+	Dcor.js - JavaScript function decoration based on AOP
+	
+	A simple JavaScript library for decorating functions with common and useful cross-cutting concerns like 
+	logging, authorization, caching, etc. 
+	It is heavily based on closures and functional aspects of JavaScript.
+
+	Alan Zawari 
+	May 2014
+	
+	Note: While the library has been tested in browser, 
+	      it's not tested on Node.js yet.
+		  
+	Dcor predefined aspects:
+		logged: Allows logging function calls along with the passed arguments and the return values
+		secured: Secures a function by ignoring (or breaking) unauthorized calls 
+		cached: Stores the function return value to cache (memory) and use it for later calls
+		performanceLogged: Measures the runtime duration of the function
+		singleton: Makes the function singleton. All future instances will be the same.
+		safeConstructor: Protects constructor functions against NOT using 'new'. A call will be considered as an instantiation. 
+		asynch: Makes synchronous functions asynchronous to prevent UI blocking
+		
+***********************************************************/
+
+(function (global, exporter) {
+
+    // export
+    if (typeof module !== 'undefined') {
+        module.exports = exporter(); //Node.js
+    } else {
+        global.Dcor = exporter(); //browser
+    }
+
+})(this, function () {
+    var undefined, //Guard against variable named "undefined" being declared in global context.
+    Dcor = {
+        name: "Dcor.js",
+        version: "1.0"
+    };
+
+    Dcor.decorate = function (fnNamePattern, decors, namespaces) {
+        //TODO: check for duplicate decors
+        //no namespaces: global ns
+        if (!namespaces || namespaces.length === 0) namespaces = [(function () {
+            return this;
+        }).call()]; //doesn't work in strict mode
+
+        //iterate over all namespaces 
+        namespaces.forEach(function (ns) {
+            for (var key in ns) {
+                if (key == "localStorage" || key == "sessionStorage" || key == "caches") continue; //prevent causing error when running locally (for testing)
+                if (typeof ns[key] == 'function' && ns.hasOwnProperty(key) && key.match(fnNamePattern)) {
+                    decors.forEach(function (decor) {
+                        appyDecor(ns[key], key, decor, ns);
+                        console.log(key);
+                    });
+                }
+            }
+        });
+    };
+
+    Dcor.dedecorate = null; //TODO: remove decors from functions
+
+
+    function appyDecor(fn, fnName, decor, ns) {
+        fn.__decoratedWith = decor; //for informational purposes
+        ns[fnName] = decorFactory(fn, fnName, decor, ns); //overwrite original function with proper wrapper function
+        ns[fnName].__fnName = fnName;
+        ns[fnName].__fn = fn;
+        ns[fnName].__decoratedWith = fn.__decoratedWith;
+
+        //__fnOriginal is always the original function. 
+        //it's only used to be passed to callbacks for informational purposes
+        if (fn.__fnOriginal) ns[fnName].__fnOriginal = fn.__fnOriginal;
+        else {
+            ns[fnName].__fnOriginal = fn;
+            fn.__fnOriginal = fn; //for the original function itself
+        }
+    }
+
+    function decorFactory(fn, fnName, decor, ns) {
+        if (decor.type === "logged") {
+            return function () {
+                //logged
+                //var result = ns[fnName].__fn.apply(this, arguments);
+                var result = fn.apply(this, arguments);
+                if (decor.callback) decor.callback(fnName, arguments, result, fn.__fnOriginal.toString(), ns);
+                return result;
+            };
+
+        } else if (decor.type === "secured") {
+            return function () {
+                //secured
+                if (!decor.callback || (decor.callback && decor.callback(fnName, arguments, null, fn.__fnOriginal.toString(), ns))) return fn.apply(this, arguments); //authorized to run function
+            };
+
+        } else if (decor.type === "cached") {
+            return function () {
+                //cached
+                fn.__cache = fn.__cache || {};
+                //var argsStr = Array.prototype.slice.call(arguments).join(",");
+                var argsStr = ""; //no arguments
+                for (var i = 0; i < arguments.length; i++) {
+                    if (typeof arguments[i] == "object") argsStr += JSON.stringify(arguments[i]) + ",";
+                    else argsStr += arguments[i].toString() + ",";
+                }
+
+                if (fn.__cache[argsStr] != null) {
+                    if (decor.callback) decor.callback(true, fnName, arguments, fn.__cache[argsStr], fn.__fnOriginal.toString(), ns);
+                    return fn.__cache[argsStr];
+                } else {
+                    var result = fn.apply(this, arguments);
+                    if (decor.callback) decor.callback(false, fnName, arguments, result, fn.__fnOriginal.toString(), ns);
+                    return fn.__cache[argsStr] = result;
+                }
+            };
+
+        } else if (decor.type === "performanceLogged") {
+            return function () {
+                //performanceLogged
+                var startTime = (new Date()).getTime(),
+                    result = fn.apply(this, arguments),
+                    duration = (new Date()).getTime() - startTime;
+                if (decor.callback) decor.callback(duration, fnName, arguments, result, fn.__fnOriginal.toString(), ns);
+                return result;
+            };
+        } else if (decor.type === "singleton") {
+            return function () {
+                //singleton
+                if (fn.__singletonInstance) return fn.__singletonInstance;
+                fn.__singletonInstance = this;
+                return fn.apply(this, arguments);
+            };
+        } else if (decor.type === "safeConstructor") { //'new' keyword will be used even if the function is called normally
+            return function () {
+                //safeConstructor
+                if (!(this instanceof fn)) //called as a function (without 'new')
+                return new(Function.prototype.bind.apply(fn, Array.prototype.concat.apply([null], arguments)));
+                else //instanciated with 'new' keyword
+                return fn.apply(this, arguments);
+            };
+        } else if (decor.type === "asynch") {
+            return function () {
+                //asynch
+                var that = this,
+                    args = arguments;
+                setTimeout(function () {
+                    var result = fn.apply(that, args);
+                    if (decor.callback) decor.callback(fnName, args, result, fn.__fnOriginal.toString(), ns);
+                }, 0);
+            };
+        }
+    }
+
+    //easy-access functions with predefined decor types
+    Dcor.logged = function (fnNamePattern, decorCallback, namespaces) {
+        return Dcor.decorate(fnNamePattern, [{
+            type: "logged",
+            callback: decorCallback
+        }], namespaces);
+    };
+
+    Dcor.secured = function (fnNamePattern, decorCallback, namespaces) {
+        return Dcor.decorate(fnNamePattern, [{
+            type: "secured",
+            callback: decorCallback
+        }], namespaces);
+    };
+
+    Dcor.cached = function (fnNamePattern, decorCallback, namespaces) {
+        return Dcor.decorate(fnNamePattern, [{
+            type: "cached",
+            callback: decorCallback
+        }], namespaces);
+    };
+
+    Dcor.performanceLogged = function (fnNamePattern, decorCallback, namespaces) {
+        return Dcor.decorate(fnNamePattern, [{
+            type: "performanceLogged",
+            callback: decorCallback
+        }], namespaces);
+    };
+
+    Dcor.singleton = function (fnNamePattern, decorCallback, namespaces) {
+        return Dcor.decorate(fnNamePattern, [{
+            type: "singleton",
+            callback: decorCallback
+        }], namespaces);
+    };
+
+    Dcor.safeConstructor = function (fnNamePattern, decorCallback, namespaces) {
+        return Dcor.decorate(fnNamePattern, [{
+            type: "safeConstructor",
+            callback: decorCallback
+        }], namespaces);
+    };
+
+    Dcor.asynch = function (fnNamePattern, decorCallback, namespaces) {
+        return Dcor.decorate(fnNamePattern, [{
+            type: "asynch",
+            callback: decorCallback
+        }], namespaces);
+    };
+
+    return Dcor;
+});
